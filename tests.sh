2718d3de47708b9443dfb2d652d12ad78033bdb3
LIParadise <a58524andy@gmail.com>
2018-11-21 19:54:17 +0800

    added test script (in bash format, zsh could not handle)

---
+++ tests.sh
@@ -0,0 +1,51 @@
+if [ $# -ne 1 ]; then
+  echo "usage: ./tests.sh <a/b/d>"
+  exit
+fi
+
+if [ ${1} = a ]
+then
+  echo "case array"
+  ref/adtTest.array -f tests/do1 >& log/ref1
+  ref/adtTest.array -f tests/do2 >& log/ref2
+  ref/adtTest.array -f tests/do3 >& log/ref3
+  ref/adtTest.array -f tests/do4 >& log/ref4
+  echo "ref done"
+  ./adtTest.array     -f tests/do1 >& log/my1
+  ./adtTest.array     -f tests/do2 >& log/my2
+  ./adtTest.array     -f tests/do3 >& log/my3
+  ./adtTest.array     -f tests/do4 >& log/my4
+  echo "mine done"
+  echo ""
+elif [ ${1} = b ]
+then
+  echo "case bst"
+  ref/adtTest.bst -f tests/do1 >& log/ref1
+  ref/adtTest.bst -f tests/do2 >& log/ref2
+  ref/adtTest.bst -f tests/do3 >& log/ref3
+  ref/adtTest.bst -f tests/do4 >& log/ref4
+  echo "ref done"
+  ./adtTest.bst     -f tests/do1 >& log/my1
+  ./adtTest.bst     -f tests/do2 >& log/my2
+  ./adtTest.bst     -f tests/do3 >& log/my3
+  ./adtTest.bst     -f tests/do4 >& log/my4
+  echo "mine done"
+  echo ""
+elif [ ${1} = d ]
+then
+  echo "case dlist"
+  ref/adtTest.dlist -f tests/do1 >& log/ref1
+  ref/adtTest.dlist -f tests/do2 >& log/ref2
+  ref/adtTest.dlist -f tests/do3 >& log/ref3
+  ref/adtTest.dlist -f tests/do4 >& log/ref4
+  echo "ref done"
+  ./adtTest.dlist     -f tests/do1 >& log/my1
+  ./adtTest.dlist     -f tests/do2 >& log/my2
+  ./adtTest.dlist     -f tests/do3 >& log/my3
+  ./adtTest.dlist     -f tests/do4 >& log/my4
+  echo "mine done"
+  echo ""
+else
+  exit
+fi
+exit
