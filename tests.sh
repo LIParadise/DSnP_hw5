89ed4007e1b3182876a330846be09698b536881f
LIParadise <a58524andy@gmail.com>
2018-11-25 18:53:17 +0800

    huge test material, do6, passed (dlist)

--- tests.sh
+++ tests.sh
@@ -10,11 +10,13 @@ then
   ref/adtTest.array -f tests/do2 >& log/ref2
   ref/adtTest.array -f tests/do3 >& log/ref3
   ref/adtTest.array -f tests/do4 >& log/ref4
+  ref/adtTest.array -f tests/do6 >& log/ref6
   echo "ref done"
   ./adtTest.array     -f tests/do1 >& log/my1
   ./adtTest.array     -f tests/do2 >& log/my2
   ./adtTest.array     -f tests/do3 >& log/my3
   ./adtTest.array     -f tests/do4 >& log/my4
+  ./adtTest.array     -f tests/do6 >& log/my6 
   echo "mine done"
   echo ""
 elif [ ${1} = b ]
@@ -24,11 +26,13 @@ then
   ref/adtTest.bst -f tests/do2 >& log/ref2
   ref/adtTest.bst -f tests/do3 >& log/ref3
   ref/adtTest.bst -f tests/do4 >& log/ref4
+  ref/adtTest.bst -f tests/do6 >& log/ref6
   echo "ref done"
   ./adtTest.bst     -f tests/do1 >& log/my1
   ./adtTest.bst     -f tests/do2 >& log/my2
   ./adtTest.bst     -f tests/do3 >& log/my3
   ./adtTest.bst     -f tests/do4 >& log/my4
+  ./adtTest.bst     -f tests/do6 >& log/my6 
   echo "mine done"
   echo ""
 elif [ ${1} = d ]
@@ -38,11 +42,13 @@ then
   ref/adtTest.dlist -f tests/do2 >& log/ref2
   ref/adtTest.dlist -f tests/do3 >& log/ref3
   ref/adtTest.dlist -f tests/do4 >& log/ref4
+  ref/adtTest.dlist -f tests/do6 >& log/ref6
   echo "ref done"
   ./adtTest.dlist     -f tests/do1 >& log/my1
   ./adtTest.dlist     -f tests/do2 >& log/my2
   ./adtTest.dlist     -f tests/do3 >& log/my3
   ./adtTest.dlist     -f tests/do4 >& log/my4
+  ./adtTest.dlist     -f tests/do6 >& log/my6 
   echo "mine done"
   echo ""
 else
