43f4e74090e621cae93b616fc1ca251be4df8eec
LIParadise <a58524andy@gmail.com>
2019-10-30 17:07:00 +0800

    change testing method

--- src/generate.cpp
+++ src/generate.cpp
@@ -6,22 +6,29 @@
 #include <immintrin.h>
 #include <cassert>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+union My64Bit {
+  unsigned long long tmp_u_LL;
+  char charArr[8];
+}my64Bit;
+
+static const int mySize = 150 * 10000;
+
 int main(){
   vector<string> outputStr;
-  outputStr.reserve(100*10000);
-  queue<unsigned long long> tmpStorage;
-  for( int i = 0; i < 100*10000; ++ i )
-    outputStr.push_back( "adta -r " );
+  outputStr.reserve(mySize);
+  queue<string> tmpStorage;
+  for( int i = 0; i < mySize; ++ i )
+    outputStr.push_back( "adta -s " );
 
-  unsigned long long tmp_u_LL;
-  for( int i = 0, j = 0; i < 100*10000; ++i ){
+  for( int i = 0, j = 0; i < mySize; ++i ){
     if( j > i/2 ){
-      _rdrand64_step(&tmp_u_LL);
-      if( tmp_u_LL %2 ){
-        outputStr.at(i).assign(string("adtd -r "));
+      _rdrand64_step(&(my64Bit.tmp_u_LL));
+      if( my64Bit.tmp_u_LL %2 ){
+        outputStr.at(i).assign(string("adtd -s "));
         j--;
         continue;
       }
@@ -31,11 +38,13 @@ int main(){
 
   for( auto& i : outputStr ){
     if( i[3] == 'a' ){
-      _rdrand64_step(&tmp_u_LL);
-      tmpStorage.push(tmp_u_LL);
-      i.append(to_string(tmpStorage.back()%(65536*2)));
+      _rdrand64_step(&(my64Bit.tmp_u_LL));
+      for( int j = 0; j < 8; ++ j)
+        my64Bit.charArr[j] = (  ((unsigned)my64Bit.charArr[j])%26  ) + 'a';
+      tmpStorage.push( string( my64Bit.charArr , 6 ) );
+      i.append(tmpStorage.back());
     }else if( i[3] == 'd' ){
-      i.append(to_string(tmpStorage.front()%(65536*2)));
+      i.append(tmpStorage.front());
       tmpStorage.pop();
     }else {
       assert( 0 && "wtf" );
